f7ce5aecb97ab467b99f09d91b1e272f55c1b630
juanpedroarmella <juanpedroarmella@gmail.com>
2020-11-25 03:26:00 -0300

    Clase 5 - Ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { BeerListComponent } from './beer-list/beer-list.component';
 
 
 import { FormsModule } from '@angular/forms';
+import { BreweryAboutComponent } from './brewery-about/brewery-about.component';
+import { BreweryBeersComponent } from './brewery-beers/brewery-beers.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeerListComponent
+    BeerListComponent,
+    BreweryAboutComponent,
+    BreweryBeersComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
